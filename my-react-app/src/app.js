61f17a2a77553b0416f2bf8a5a6a785561f19769
rabbi-hosen <rabbihosen01581@gmail.com>
2023-09-24 17:50:01 +0600

    useState hooks

---
+++ my-react-app/src/app.js
@@ -0,0 +1,48 @@
+
+import React from 'react'
+
+import HOOKS_USESTATE2 from './HOOKS_USESTATE/index2'
+
+
+export default function App() {
+    return (
+        <div>
+          
+            <HOOKS_USESTATE2 />
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import React from 'react';
+import Card from './components/Card';
+import Data from './data.json'
+
+function App (){
+
+  
+
+
+   return <div>
+
+            <h1 className='headingStyle'> Todo App</h1>
+           { Data.map( (item, index) => <  Card key={index} titleText = {item.title} descText = {item.desc} /> )}
+    
+   </div>
+}
+
+
+export default App;*/
